1d809709ab490269120950d5a027479eca95ebd4
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-05-29 16:15:53 +0530

    added cv and contact form connected

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,11 @@ import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
+import Swal from "sweetalert2";
+
+//
+//
+//
 
 const Contact = () => {
   const formRef = useRef();
@@ -27,41 +32,76 @@ const Contact = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
+    //if empty
 
-    emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        {
-          from_name: form.name,
-          to_name: "JavaScript Mastery",
-          from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
-          message: form.message,
-        },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-      )
-      .then(
-        () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+    if (form.name === "") {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "How Do I recognise you without a name? 😔",
+      });
+      setLoading(false);
+    }
+    if (form.email === "") {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "I need your mail address to get back to you! 😎",
+      });
+      setLoading(false);
+      return;
+    }
+    if (form.message === "") {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Don't you have anything to say? 🥺",
+      });
+      setLoading(false);
+      return;
+    }
 
-          setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
+    //if !empty
+    const email = await emailjs.send(
+      "service_rflrbb8",
+      "template_kfd5lcd",
+      {
+        from_name: form.name,
+        to_name: "ishara",
+        from_email: form.email,
+        to_email: "hello.isharamadusanka@gmail.com",
+        message: form.message,
+      },
+      "cndTcn_G7HAC5EjRG"
+    );
+    if (email) {
+      try {
+        setLoading(false);
 
-          alert("Ahh, something went wrong. Please try again.");
-        }
-      );
+        Swal.fire(
+          "Thank You!",
+          "I will get back to you as soon as possible",
+          "success"
+        );
+        setForm({
+          name: "",
+          email: "",
+          message: "",
+        });
+      } catch (error) {
+        setLoading(false);
+        console.error(error);
+
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: error.message,
+        });
+      }
+    }
   };
 
   return (
@@ -73,7 +113,7 @@ const Contact = () => {
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
       >
         <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact.</h3>
+        <h3 className={styles.sectionHeadText}>Contact</h3>
 
         <form
           ref={formRef}
@@ -87,18 +127,18 @@ const Contact = () => {
               name="name"
               value={form.name}
               onChange={handleChange}
-              placeholder="What's your good name?"
+              placeholder="What's your name?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
           <label className="flex flex-col">
             <span className="text-white font-medium mb-4">Your email</span>
             <input
-              type="email"
+              type="text"
               name="email"
               value={form.email}
               onChange={handleChange}
-              placeholder="What's your web address?"
+              placeholder="What's your email address?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
@@ -109,7 +149,7 @@ const Contact = () => {
               name="message"
               value={form.message}
               onChange={handleChange}
-              placeholder="What you want to say?"
+              placeholder="What do you want to say?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
